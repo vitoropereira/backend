5370fcc3f28e0f2847098075f84454f58280d707
Vitor <vop1234@hotmail.com>
2020-06-08 20:11:29 -0300

    Using query parameters

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -6,15 +6,15 @@ import ListProviderAppointmentsService from '@modules/appointments/services/List
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id
-    const { day, month, year } = request.body
+    const { day, month, year } = request.query
 
     const listProviderAppointmentsService = container.resolve(ListProviderAppointmentsService)
 
     const appointments = await listProviderAppointmentsService.execute({
-      day,
-      month,
-      year,
-      provider_id
+      provider_id,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year)
     })
 
     return response.json(appointments)
